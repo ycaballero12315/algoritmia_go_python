e93c38f04f64e1a1be2f8c18a1204d5c3582e719
Yoeny Caballero Gonzalez <ycaballero12315@gmail.com>
2025-10-13 19:51:29 -0300

    paquete principal de goland

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"algoritmia/algoritmia"
+	"fmt"
+)
+
+func main()  {
+	fmt.Printf("La potencia es: %d\n", algoritmia.Potencia(2,5))
+	fmt.Printf("El factorial se %d\n", algoritmia.Factorial(5))
+}
