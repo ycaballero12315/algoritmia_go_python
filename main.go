83d5b97fc724abaf4ae8634f276e0a3a4d8e94e5
Yoeny Caballero Gonzalez <ycaballero12315@gmail.com>
2025-10-14 00:04:01 -0300

    agregado del print de sort

--- main.go
+++ main.go
@@ -10,4 +10,5 @@ func main() {
 	fmt.Printf("El factorial se %d\n", algoritmia.Factorial(5))
 	fmt.Printf("Algoritmo divide y venceras: %d\n", algoritmia.DivVenceras([]int{2, 5, 6, 7, 23}, 0, 4))
 	fmt.Printf("El metodo extranno Ackerman: %d\n", algoritmia.Ackermann(1, 2))
+	fmt.Printf("El menor numero esta en posicion: %d\n", algoritmia.Selection_sort([]int{2, 5, 6, 7, 23}, 0))
 }
