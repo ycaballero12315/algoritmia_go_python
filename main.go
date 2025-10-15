e8529e7d7aea7ca5aed9b28e9e2b02a56b5e698f
Yoeny Caballero Gonzalez <ycaballero12315@gmail.com>
2025-10-15 17:59:40 -0300

    add stack and contruyed new stack

--- main.go
+++ main.go
@@ -11,4 +11,5 @@ func main() {
 	fmt.Printf("Algoritmo divide y venceras: %d\n", algoritmia.DivVenceras([]int{2, 5, 6, 7, 23}, 0, 4))
 	fmt.Printf("El metodo extranno Ackerman: %d\n", algoritmia.Ackermann(1, 2))
 	fmt.Printf("El menor numero esta en posicion: %d\n", algoritmia.Selection_sort([]int{2, 5, 6, 7, 23}, 0))
+	fmt.Printf("El fibonassi es: %d\n", algoritmia.Fibonnacci(5))
 }
