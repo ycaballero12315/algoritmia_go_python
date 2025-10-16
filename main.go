bd34fea949f473d1ff1a716f1c7462da97545047
Yoeny Caballero Gonzalez <ycaballero12315@gmail.com>
2025-10-16 14:19:55 -0300

    update main

--- main.go
+++ main.go
@@ -12,4 +12,10 @@ func main() {
 	fmt.Printf("El metodo extranno Ackerman: %d\n", algoritmia.Ackermann(1, 2))
 	fmt.Printf("El menor numero esta en posicion: %d\n", algoritmia.Selection_sort([]int{2, 5, 6, 7, 23}, 0))
 	fmt.Printf("El fibonassi es: %d\n", algoritmia.Fibonnacci(5))
+	fmt.Printf("Elemento insertado en la lista %d\n", algoritmia.AddElms(40))
+	fmt.Printf("Elemento eliminado: %d\n", algoritmia.PopElm())
+	fmt.Printf("Existe el numero 20?: %v\n", algoritmia.IsNumExist(20))
+	fmt.Printf("Tamanno de la lista: %d\n", algoritmia.Length())
+	algoritmia.AddElms(40)
+	fmt.Printf("El ultimo elemento es: %d\n", algoritmia.PopElm())
 }
